b7ed516cdd49c47e957b840b4da5a1fc9622a1c0
Henrique <henriquebpmteste@gmail.com>
2026-01-28 15:14:18 -0300

    Backup automatico

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app.component'; // Caminho corrigido
+import { AppComponent } from './app.component'; // Caminho corrigido (sem /app)
 import { provideZonelessChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
-import { routes } from './app.routes'; // Caminho corrigido
+import { routes } from './app.routes'; // Caminho corrigido (sem /app)
 import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideZonelessChangeDetection(),
     provideRouter(routes, withHashLocation()), 
+    
+    // Configuração do PWA para Android
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000'
-    }), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          })
+    })
   ]
 }).catch((err) => console.error(err));
